c87e3483498a3901c5ae80d7a04fda6b7d7ffebe
Michael Scragg <mike.scragg@gmail.com>
2016-08-02 21:17:19 -0500

    Weather app in React Redux

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '4c38a66d760f38b01a3e9fb83f29a6a9';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
