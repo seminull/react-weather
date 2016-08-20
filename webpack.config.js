2d4894c55c584cbd43568d7a297a4aca4cc586e2
Michael Scragg <mike.scragg@gmail.com>
2016-08-19 20:32:34 -0500

    React flexboxgrid webpack config changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,8 @@
+const path = require("path");
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
 module.exports = {
   entry: [
     './src/index.jsx'
@@ -14,13 +19,34 @@ module.exports = {
       query: {
         presets: ['react', 'es2015', 'stage-1']
       }
+    }, {
+      test: /\.(scss|css)$/,
+      loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss!sass?sourceMap'),
+      exclude: /flexboxgrid/
+    }, {
+      test: /\.css$/,
+      loader: 'style!css?modules',
+      include: /flexboxgrid/,
     }]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: [ '', '.js', '.jsx', '.scss', '.json', '.node' ]
   },
   devServer: {
     historyApiFallback: true,
     contentBase: './'
+  },
+  plugins: [
+    new ExtractTextPlugin('bundle.css', { allChunks: true }),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    })
+  ],
+  postcss: [autoprefixer],
+  sassLoader: {
+    data: '@import "theme/_config.scss";',
+    includePaths: [path.resolve(__dirname, './src')]
   }
 };
